c86fc69bdfa45b18cb76606b37a66863431890ce
Andrew William Watson <aww@awwsmm.com>
2023-12-06 17:01:44 -0500

    move parse_http_request into a library, remove regex dependency, add some tests

---
+++ ping/src/lib.rs
@@ -0,0 +1,86 @@
+use std::collections::HashMap;
+use std::io;
+
+pub fn parse_http_request(reader: &mut impl io::BufRead) -> Result<(String, HashMap<String, String>, Option<String>), io::Error> {
+    let mut request = String::new();
+    reader.read_line(&mut request)?;
+
+    let mut headers: HashMap<String, String> = HashMap::new();
+
+    loop {
+        let mut line = String::new();
+        match reader.read_line(&mut line) {
+            Ok(size) if size > 2 => { // a blank line (CRLF only) separates HTTP headers and body
+                match line.split_once(": ") { // HTTP headers are always formatted as "key: value"
+                    Some((key, value)) => headers.insert(key.trim().into(), value.trim().into()),
+                    None => continue, // skip any header lines that can't be parsed
+                };
+            },
+            _ => break // if the reader fails to read the next line, quit early
+        };
+    }
+
+    let mut body: Option<String> = None;
+
+    if let Some(length) = headers.get("Content-Length") {
+        if let Ok(length) = length.parse::<usize>() {
+            let mut buffer = vec![0; length];
+            reader.read_exact(&mut buffer).unwrap();
+            body = Some(std::str::from_utf8(buffer.as_slice()).unwrap().into());
+        }
+    }
+
+    Ok((request.trim().into(), headers, body))
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_parse_http_request_no_body() {
+        let mut request = "\
+POST /pong HTTP/1.1
+Host: 127.0.0.1:7878
+User-Agent: curl/8.1.2
+Accept: */*
+        ";
+
+        let (request, headers, body) = parse_http_request(&mut request.as_bytes()).unwrap();
+
+        assert_eq!(request, "POST /pong HTTP/1.1");
+
+        assert_eq!(headers.get("Host").unwrap(),       "127.0.0.1:7878");
+        assert_eq!(headers.get("User-Agent").unwrap(), "curl/8.1.2");
+        assert_eq!(headers.get("Accept").unwrap(),     "*/*");
+
+        assert_eq!(body, None);
+    }
+
+    #[test]
+    fn test_parse_http_request_with_body() {
+        let mut request = "\
+POST /pong HTTP/1.1
+Host: 127.0.0.1:7878
+User-Agent: curl/8.1.2
+Accept: */*
+Content-Length: 13
+Content-Type: application/x-www-form-urlencoded
+
+Hello, World!
+        ";
+
+        let (request, headers, body) = parse_http_request(&mut request.as_bytes()).unwrap();
+
+        assert_eq!(request, "POST /pong HTTP/1.1");
+
+        assert_eq!(headers.get("Host").unwrap(),           "127.0.0.1:7878");
+        assert_eq!(headers.get("User-Agent").unwrap(),     "curl/8.1.2");
+        assert_eq!(headers.get("Accept").unwrap(),         "*/*");
+        assert_eq!(headers.get("Content-Length").unwrap(), "13");
+        assert_eq!(headers.get("Content-Type").unwrap(),   "application/x-www-form-urlencoded");
+
+        assert_eq!(body.unwrap(), "Hello, World!");
+    }
+
+}
--- ping/src/main.rs
+++ ping/src/main.rs
@@ -1,7 +1,8 @@
-use std::io::{BufRead, BufReader, Read, Write};
-use std::net::{TcpListener, TcpStream};
 use std::{thread, time};
-use regex::Regex;
+use std::io::{BufReader, Write};
+use std::net::{TcpListener, TcpStream};
+
+use ping::parse_http_request;
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
@@ -16,34 +17,27 @@ fn main() {
 fn handle_connection(mut stream: TcpStream) {
     let mut reader = BufReader::new(&mut stream);
 
-    let re = Regex::new(r"Content-Length: (?<length>\d+)").unwrap();
+    match parse_http_request((&mut reader).into()) {
+        Ok((request, headers, body)) => {
+            if request == "POST /pong HTTP/1.1" {
+                respond(
+                    Ok("pong received, sending ping"),
+                    &mut stream
+                );
 
-    let mut line = String::new();
-    let mut length: usize = 0;
+                println!("sender is {}", body.unwrap_or(String::from("unknown")));
 
-    let mut request_line = String::new();
-    reader.read_line(&mut request_line).unwrap();
+            } else {
+                respond(
+                    Err("unrecognized request received"),
+                    &mut stream
+                );
 
-    loop {
-        length = match reader.read_line(&mut line) {
-            Err(_) => break,
-            Ok(size) if size < 3 => break,
-            Ok(_) => {
-                match re.captures(line.as_str()) {
-                    None => 0,
-                    Some(caps) => caps["length"].parse::<usize>().unwrap()
-                }
+                println!("sender is {}", body.unwrap_or(String::from("unknown")));
             }
-        }
+        },
+        Err(_) => println!("Could not parse http request")
     }
-
-    let mut buffer = vec![0; length];
-    reader.read_exact(&mut buffer).unwrap();
-    let body: &str = std::str::from_utf8(&buffer).unwrap();
-
-    println!("body = {}", body);
-
-    thread::sleep(time::Duration::from_secs(1));
 
     fn respond(response: Result<&str, &str>, stream: &mut TcpStream) {
         let (status, msg) = match response {
@@ -54,26 +48,9 @@ fn handle_connection(mut stream: TcpStream) {
         let len = msg.len();
         let response = format!("{status}\r\nContent-Length: {len}\r\n\r\n{msg}");
 
+        thread::sleep(time::Duration::from_secs(1));
+
         stream.write_all(response.as_bytes()).unwrap();
-    }
-
-    println!("req = {}", request_line);
-
-    if request_line == "POST /pong HTTP/1.1\r\n" {
-        respond(
-            Ok("pong received, sending ping"),
-            &mut stream
-        );
-
-        println!("sender is {}", body);
-
-    } else {
-        respond(
-            Err("unrecognized request received"),
-            &mut stream
-        );
-
-        println!("sender is {}", body);
     }
 
 }
